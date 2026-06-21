35a8ad83f5ef77822cac8d3ea04abb41da558357
tjnzack-cyber <tjnzack@gmail.com>
2026-06-21 21:11:19 +0200

    Add image remote patterns to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['indieaxis-official-v1.loca.lt', '3000-i3cra1d1e6w8b9f22snyc.e2b.app'],
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
